70d7e17f4665fda7f61527b18e27e5c36bbe3c01
saikiranRoyal <saikiranroyal111@gmail.com>
2023-02-01 12:03:57 +0530

    Form implemented with personal details

--- src/app/components/createstudents/createstudents.component.ts
+++ src/app/components/createstudents/createstudents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-createstudents',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./createstudents.component.css']
 })
 export class CreatestudentsComponent {
-
+   public createstudentForm:FormGroup=new FormGroup(
+    {
+      name:new FormControl(),
+      gender:new FormControl(),
+      mobile:new FormControl(),
+      email:new FormControl(),
+      batch:new FormControl(),
+    }
+   )
+   submit(){
+    console.log(this.createstudentForm.value)
+   }
 }
